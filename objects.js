2ed3fd2e4dfd3ae3d252359d8225aded180c3f9a
Your Name <ckoutianov@mcad.edu>
2026-01-29 19:10:13 -0800

    Create objects.js

---
+++ objects.js
@@ -0,0 +1,15 @@
+const myGames = [
+    {
+        name: "Uno",
+        rating: 8,
+        owned: true,
+        famFavorite: true
+    },
+    {
+        name: "Mancala",
+        rating: 6,
+        owned: true,
+        famFavorite: false
+    }
+]
+console.log(myGames);
